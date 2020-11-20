c1b8f1c14ad931c2336dd3f7d2f330822a371532
Joao Silva <josejoaosilva.n@gmail.com>
2020-11-20 08:48:54 -0300

    Remove swiftlint warnings

--- StoreAPP/SceneDelegate.swift
+++ StoreAPP/SceneDelegate.swift
@@ -6,16 +6,14 @@
 //
 
 import UIKit
+// swiftlint:disable line_length unused_optional_binding
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
@@ -50,6 +48,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
-
 }
-
