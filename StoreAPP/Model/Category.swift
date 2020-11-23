7095ef2d07b56f645d2d57a82c6764c26908b871
All3yp <noyalle@ualberta.ca>
2020-11-23 11:40:28 -0300

    feature/18

---
+++ StoreAPP/Model/Category.swift
@@ -0,0 +1,60 @@
+//
+//  Category.swift
+//  StoreAPP
+//
+//  Created by Alley Pereira on 23/11/20.
+//
+
+import UIKit
+
+enum Category: Int {
+    case drinks
+    case candies
+    case dry
+    case pickled
+    case spices
+    case vegetables
+    case fruit
+    case others
+    
+    var color: UIColor {
+        switch self {
+        case .drinks:
+            return UIColor.systemOrange
+        case .candies:
+            return UIColor.systemPink
+        case .dry:
+            return UIColor.init(named: "othersColor")!
+        case .pickled:
+            return UIColor.systemTeal
+        case .spices:
+            return UIColor.systemYellow
+        case .vegetables:
+            return UIColor.systemGreen
+        case .others:
+            return UIColor.systemIndigo
+        case .fruit:
+            return UIColor.systemRed
+        }
+    }
+    var icon: UIImage {
+        switch self {
+        case .drinks:
+            return UIImage.init(named: "drinks")!
+        case .candies:
+            return UIImage.init(named: "candies")!
+        case .dry:
+            return UIImage.init(named: "dry")!
+        case .pickled:
+            return UIImage.init(named: "pickled")!
+        case .spices:
+            return UIImage.init(named: "spices")!
+        case .vegetables:
+            return UIImage.init(named: "vegetables")!
+        case .fruit:
+            return UIImage.init(named: "fruit")!
+        case .others:
+            return UIImage.init(named: "others")!
+        }
+    }
+}
