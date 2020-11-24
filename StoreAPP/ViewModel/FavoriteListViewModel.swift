d3a89c55f64799995d993eaedbcce3586b1d398e
Fernando <fernanddolucas@gmail.com>
2020-11-24 10:00:21 -0300

    Created favorite and product list view model

---
+++ StoreAPP/ViewModel/FavoriteListViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  FavoriteListViewModel.swift
+//  StoreAPP
+//
+//  Created by Fernando de Lucas da Silva Gomes on 24/11/20.
+//
+
+import Foundation
+
+struct FavoriteListViewModel{
+    
+   // let productList : [ProductViewModel] = []
+   // let repository = ProductRepository()
+    
+}
+
+extension FavoriteListViewModel{
+    
+//    public var numberOfProducts : Int {
+//        return self.productList.count
+//    }
+//
+//    public func productForCell(at index: Int) -> ProductViewModel? {
+//        if numberOfProducts >= index {
+//            let product = productList[index]
+//            return product
+//        } else { return nil}
+//    }
+}
---
+++ StoreAPP/ViewModel/ProductListViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  ProductListViewModel.swift
+//  StoreAPP
+//
+//  Created by Fernando de Lucas da Silva Gomes on 24/11/20.
+//
+
+import Foundation
+
+struct ProductListViewModel{
+    
+   // let productList : [ProductViewModel] = []
+   // let repository = ProductRepository()
+    
+}
+
+extension ProductListViewModel{
+    
+//    public var numberOfProducts : Int {
+//        return self.productList.count
+//    }
+//
+//    public func productForCell(at index: Int) -> ProductViewModel? {
+//        if numberOfProducts >= index {
+//            let product = productList[index]
+//            return product
+//        } else { return nil}
+//    }
+    
+    //public func deleteFromCell(at index: Int) -> ProductViewModel{
+        //        if numberOfProducts >= index {
+        //            let product = productList[index]
+        //              repository.delete(product)
+        //            return product
+        //        } else { return nil}
+        //    }
+    
+    //public func favoriteFromCell(at index: Int) -> ProductViewModel{
+        //        if numberOfProducts >= index {
+        //            let product = productList[index]
+        //              repository.favorite(product)
+        //            return product
+        //        } else { return nil}
+        //    }
+    
+    
+}
+
+
