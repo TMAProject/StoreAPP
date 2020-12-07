bb1f5c65785cc8cc11676ebe7df7b0a48788ac88
Fernando <fernanddolucas@gmail.com>
2020-12-07 09:49:08 -0300

    Creted test class for ProductForm

---
+++ StoreAPPTests/ProductFormTest.swift
@@ -0,0 +1,63 @@
+//
+//  ProductFormTest.swift
+//  StoreAPPTests
+//
+//  Created by Fernando de Lucas da Silva Gomes on 07/12/20.
+//
+
+import XCTest
+@testable import StoreAPP
+
+class ProductFormTest: XCTestCase {
+
+    let productForm = ProductForm.self
+    let productFields = ProductFields.self
+
+    func test_section_CreateSectionEqualToGivenArray() {
+        let createArray = productForm.create.sections
+        XCTAssertEqual(["", "Estoque"], createArray)
+    }
+
+    func test_section_EditSectionEqualToGivenArray() {
+        let createArray = productForm.edit.sections
+        XCTAssertEqual(["", "Estoque"], createArray)
+    }
+
+    func test_section_EditNumberOfSectionsEqualToGivenValue() {
+        let numberOfSections = productForm.edit.numberOfSections
+        XCTAssertEqual(2, numberOfSections)
+    }
+
+    func test_section_CreateNumberOfSectionsEqualToGivenValue() {
+        let numberOfSections = productForm.create.numberOfSections
+        XCTAssertEqual(2, numberOfSections)
+    }
+
+    func test_section_CreateFieldsEqualToGivenValue() {
+        let dictionaryField = productForm.create.fields
+        XCTAssertEqual(["": ["Nome"], "Estoque": ["Quantidade", "Quantidade ideal"]], dictionaryField)
+    }
+
+    func test_section_EditFieldsEqualToGivenValue() {
+        let dictionaryField = productForm.edit.fields
+        XCTAssertEqual(["": ["Nome"], "Estoque": ["Quantidade", "Quantidade ideal"]], dictionaryField)
+    }
+
+    func test_keyBoardType_KeyboardTypeForNameEqualToGivenKeyboardType() {
+        let keyboardType = productFields.name.keyboardType
+        let givenKeyboardType = UIKeyboardType.default
+        XCTAssertEqual(keyboardType, givenKeyboardType)
+    }
+
+    func test_keyBoardType_KeyboardTypeForQuantityEqualToGivenKeyboardType() {
+        let keyboardType = productFields.quantity.keyboardType
+        let givenKeyboardType = UIKeyboardType.numberPad
+        XCTAssertEqual(keyboardType, givenKeyboardType)
+    }
+
+    func test_keyBoardType_KeyboardTypeForIdealQuantityEqualToGivenKeyboardType() {
+        let keyboardType = productFields.idealQuantity.keyboardType
+        let givenKeyboardType = UIKeyboardType.numberPad
+        XCTAssertEqual(keyboardType, givenKeyboardType)
+    }
+}
